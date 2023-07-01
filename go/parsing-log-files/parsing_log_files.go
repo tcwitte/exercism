cf48017d1fbce1b204e08fde8f60d73c57cddf93
Taco Witte <Taco.Witte@rabobank.nl>
2023-07-01 22:10:04 +0200

    feat: parsing log files

---
+++ go/parsing-log-files/parsing_log_files.go
@@ -0,0 +1,47 @@
+package parsinglogfiles
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var validLine *regexp.Regexp = regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+var lineSeparator *regexp.Regexp = regexp.MustCompile(`<[~\*=\-]*>`)
+var passwordPattern *regexp.Regexp = regexp.MustCompile(`".*[pP][aA][sS][sS][wW][oO][rR][dD].*"`)
+var endOfLineText *regexp.Regexp = regexp.MustCompile(`end\-of\-line[\d]+`)
+var userPattern *regexp.Regexp = regexp.MustCompile(`User\s+(\w+)`)
+
+func IsValidLine(text string) bool {
+	return validLine.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	return lineSeparator.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	count := 0
+	for _, l := range lines {
+		if passwordPattern.MatchString(l) {
+			count++
+		}
+	}
+	return count
+}
+
+func RemoveEndOfLineText(text string) string {
+	return endOfLineText.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	result := make([]string, len(lines))
+	copy(result, lines)
+	for i, l := range lines {
+		matches := userPattern.FindStringSubmatch(l)
+		if matches != nil {
+			result[i] = fmt.Sprintf("[USR] %s %s", matches[1], l)
+		}
+	}
+
+	return result
+}
